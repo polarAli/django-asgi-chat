75b566b3d67a1d4cf71a62991da73cd97660bd59
AliGhotbizadeh <ali.ghotbizadeh@gmail.com>
2020-06-08 21:57:58 +0430

    fix routing.py issue

--- workshop/routing.py
+++ workshop/routing.py
@@ -1,10 +1,6 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import chat.routing
-import os
-
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'workshop.settings')
 
 
 application = ProtocolTypeRouter({
