10f4a4f19fa8201a7c0b99006a31a5d6adcbe4f4
AliGhotbizadeh <ali.ghotbizadeh@gmail.com>
2020-06-08 20:58:52 +0430

    set default DJANGO_SETTINGS_MODULE

--- workshop/routing.py
+++ workshop/routing.py
@@ -1,6 +1,11 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import chat.routing
+import os
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'workshop.settings')
+
 
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
